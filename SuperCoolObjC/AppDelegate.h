651f8f3bfe8089aa70b17219d8942018a2ade42c
Sam Pellegrene <pellegrenesam@gmail.com>
2017-04-02 10:38:53 -0500

    Initial Commit

---
+++ SuperCoolObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperCoolObjC
+//
+//  Created by Samuel Pellegrene on 4/2/17.
+//  Copyright © 2017 Samuel Pellegrene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
